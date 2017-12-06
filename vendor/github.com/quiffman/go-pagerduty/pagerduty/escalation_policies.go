6929e4d19fd9c58316ad5c72a24a457bcda1571e
Richard Guest <r.guest@gns.cri.nz>
2017-12-07 11:30:30 +1300

    Switching to PagerDuty supported go library (and CLI)

--- vendor/github.com/quiffman/go-pagerduty/pagerduty/escalation_policies.go
+++
@@ -1,8 +0,0 @@
-package pagerduty
-
-// EscalationPolicy type
-type EscalationPolicy struct {
-	ID        string `json:"id,omitempty"`
-	Name      string `json:"name,omitempty"`
-	DeletedAt string `json:"deleted_at,omitempty"`
-}
