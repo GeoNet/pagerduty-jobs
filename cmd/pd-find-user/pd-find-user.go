9aa381a03882aab51c7eed8f11c9583b0e7ed305
Richard Guest <r.guest@gns.cri.nz>
2018-09-12 11:51:18 +1200

    Updates to internal/finduser and example pd-find-user.

--- cmd/pd-find-user/pd-find-user.go
+++ cmd/pd-find-user/pd-find-user.go
@@ -7,25 +7,23 @@ import (
 	"os"
 
 	"github.com/GeoNet/pagerduty-jobs/internal/finduser"
-	"github.com/quiffman/go-pagerduty/pagerduty"
+	pagerduty "github.com/PagerDuty/go-pagerduty"
 )
 
 var (
-	subdomain string = os.Getenv("PD_SUBDOMAIN")
-	apiKey    string = os.Getenv("PD_APIKEY")
+	authToken string = os.Getenv("PD_AUTHTOKEN")
 	user      string
 )
 
 func init() {
-	flag.StringVar(&subdomain, "subdomain", subdomain, "The subdomain to be used for the Pagerduty API.")
-	flag.StringVar(&apiKey, "api-key", apiKey, "The api-key authorized for calls to the Pagerduty API.")
+	flag.StringVar(&authToken, "authtoken", authToken, "The authorization token for calls to the Pagerduty API.")
 
 	flag.StringVar(&user, "user", user, "The user ID, name or email to find and validate.")
 
 	flag.Parse()
 
-	if subdomain == "" || apiKey == "" {
-		log.Fatalln("PagerDuty subdomain and API token are required.")
+	if authToken == "" {
+		log.Fatalln("PagerDuty auth token is required.")
 	}
 
 	if user == "" {
@@ -34,9 +32,9 @@ func init() {
 }
 
 func main() {
-	pd := pagerduty.New(subdomain, apiKey)
+	pd := pagerduty.NewClient(authToken)
 
-	us := finduser.Service{*pd.Users}
+	us := finduser.Client{*pd}
 	u, err := us.FindAndValidate(user)
 	if err != nil {
 		log.Fatalln("Failed to validate user: " + err.Error())
--- internal/finduser/finduser.go
+++ internal/finduser/finduser.go
@@ -3,30 +3,30 @@ package finduser
 import (
 	"errors"
 
-	"github.com/quiffman/go-pagerduty/pagerduty"
+	pagerduty "github.com/PagerDuty/go-pagerduty"
 )
 
 var UserNotFoundError = errors.New("Could not find specified user.")
 
-type Service struct {
-	pagerduty.UsersService
+type Client struct {
+	pagerduty.Client
 }
 
-func (s *Service) FindAndValidate(in string) (*pagerduty.User, error) {
-	u, _, e := s.Get(in)
+func (c *Client) FindAndValidate(in string) (*pagerduty.User, error) {
+	u, e := c.GetUser(in, pagerduty.GetUserOptions{})
 	if e == nil && u.ID == in {
 		return u, e
 	}
-	return s.FindUser(in)
+	return c.FindUser(in)
 }
-func (s *Service) FindUser(in string) (*pagerduty.User, error) {
-	users, _, e := s.List(&pagerduty.UsersOptions{Query: in})
+func (c *Client) FindUser(in string) (*pagerduty.User, error) {
+	resp, e := c.ListUsers(pagerduty.ListUsersOptions{Query: in})
 	if e != nil {
 		return nil, e
 	}
 
-	if len(users) == 1 {
-		return &users[0], nil
+	if len(resp.Users) == 1 {
+		return &resp.Users[0], nil
 	}
 
 	return nil, UserNotFoundError
