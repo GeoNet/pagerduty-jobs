04047659d9f9071004e9449171dc5246eb6375c1
Adrian Ding <a.ding@gns.cri.nz>
2021-01-06 13:17:58 +1300

    update multi-stage docker build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -eu
 
 # Builds Docker images for the arg list.  These must be project directories
 # where this script is executed.
@@ -7,66 +7,39 @@
 # Adds the assets dir from each project to the container e.g., origin/assets
 # It is not an error for the assets dir to not exist.
 # Any assets needed by the application should be read from the assets dir
-# relative to the executable. 
+# relative to the executable.
 #
 # usage: ./build.sh project [project]
 
 if [ $# -eq 0 ]; then
-    echo Error: please supply a project to build. Usage: ./build.sh project [project]
-    exit 1
+  echo Error: please supply a project to build. Usage: ./build.sh project [project]
+  exit 1
 fi
 
-# Go Version
-GO_VERSION=${TRAVIS_GO_VERSION:-1.9}
-# Alpine Version
-ALPINE_VERSION=${ALPINE_VERSION:-3.6}
 # code will be compiled in this container
-BUILD_CONTAINER=golang:${GO_VERSION}-alpine${ALPINE_VERSION}
-GH_USER=${GH_USER:-GeoNet}
-REPO_BASE=${REPO_BASE:-quay.io/geonet}
+BUILDER_IMAGE='quay.io/geonet/golang:1.13.1-alpine'
+RUNNER_IMAGE='quay.io/geonet/alpine:3.10'
 
-DOCKER_TMP=docker-build-tmp
+VERSION='git-'$(git rev-parse --short HEAD)
 
-mkdir -p $DOCKER_TMP
-chmod +s $DOCKER_TMP
+for i in "$@"; do
+ 
+  mkdir -p cmd/$i/assets
+  dockerfile="Dockerfile"
+  if test -f "cmd/${i}/Dockerfile"; then
+    dockerfile="cmd/${i}/Dockerfile"
+  fi
 
-rm -rf $DOCKER_TMP/*
 
-VERSION='git-'`git rev-parse --short HEAD`
-
-# The current working dir to use in GOBIN etc e.g., geonet-web
-CWD=${PWD##*/}
-
-mkdir -p ${DOCKER_TMP}/etc
-
-# Assemble common resource for user.
-echo "nobody:x:65534:65534:Nobody:/:" > ${DOCKER_TMP}/etc/passwd
-
-for i in "$@"
-do
-    cmd=${i##*/}
-
-    docker run --rm \
-        -e "GOBIN=/usr/src/go/src/github.com/${GH_USER}/${CWD}/${DOCKER_TMP}" -e "GOPATH=/usr/src/go" -e "CGO_ENABLED=0" -e "GOOS=linux" -e "BUILD=$BUILD" \
-        -v "$PWD":/usr/src/go/src/github.com/${GH_USER}/${CWD} \
-        -w /usr/src/go/src/github.com/${GH_USER}/${CWD} ${BUILD_CONTAINER} \
-        go install -a -ldflags "-X main.Prefix=${i}/${VERSION}" -installsuffix cgo ./${i}
-
-    rm -f $DOCKER_TMP/Dockerfile
-
-    if [ -f $i/Dockerfile ]; then
-        cp $i/Dockerfile $DOCKER_TMP/Dockerfile
-    else
-        echo "FROM alpine:${ALPINE_VERSION}" > $DOCKER_TMP/Dockerfile
-        echo "RUN apk add --no-cache ca-certificates tzdata iputils" >> $DOCKER_TMP/Dockerfile
-        echo "ADD ./${cmd} /" >> $DOCKER_TMP/Dockerfile
-        echo "USER nobody" >> $DOCKER_TMP/Dockerfile
-        echo "ENTRYPOINT [\"/${cmd}\"]" >> $DOCKER_TMP/Dockerfile
-    fi
-
-    docker build -t ${REPO_BASE}/${cmd}:${VERSION} -f $DOCKER_TMP/Dockerfile $DOCKER_TMP
-    # tag latest.  Makes it easier to test with compose.
-    docker tag ${REPO_BASE}/${cmd}:${VERSION} ${REPO_BASE}/${cmd}:latest
+  docker build \
+    --build-arg=BUILD="$i" \
+    --build-arg=RUNNER_IMAGE="$RUNNER_IMAGE" \
+    --build-arg=BUILDER_IMAGE="$BUILDER_IMAGE" \
+    --build-arg=GIT_COMMIT_SHA="$VERSION" \
+    --build-arg=ASSET_DIR="./cmd/$i/assets" \
+    -t "quay.io/geonet/${i}:latest" \
+    -f $dockerfile .
 
 done
+
 # vim: set ts=4 sw=4 tw=0 et:
